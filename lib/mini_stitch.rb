d9c8bd175d12d0d294f7047c0be0bf778d12da9a
theminijohn <its@minijohn.me>
2017-10-30 14:58:43 +0200

    Initial v.0.1.0

--- lib/mini_stitch.rb
+++ lib/mini_stitch.rb
@@ -5,30 +5,28 @@ require 'mini_stitch/errors'
 require 'mini_stitch/version'
 
 module MiniStitch
-  class << self
-    def configuration
-      @configuration ||= Configuration.new
-    end
+  def self.configuration
+    @configuration ||= Configuration.new
+  end
 
-    def configure
-      configuration ||= Configuration.new
-      yield(configuration) if block_given?
-    end
+  def self.configure
+    self.configuration ||= Configuration.new
+    yield(configuration) if block_given?
+  end
 
-    def status
-      res = RestClient.get('https://api.stitchdata.com/v2/import/status')
+  def self.status
+    res = RestClient.get('https://api.stitchdata.com/v2/import/status')
 
-      if res.code == 200
-        return {
-          status: 200,
-          message: "Import API operating correctly on my End ~mj"
-        }
-      else
-        return {
-          status: res.code,
-          message: JSON.parse(res.body)
-        }
-      end
+    if res.code == 200
+      return {
+        status: 200,
+        message: "Import API operating correctly on my End ~mj"
+      }
+    else
+      return {
+        status: res.code,
+        message: JSON.parse(res.body)
+      }
     end
   end
 
